3c5d71a7e8a20cf5c019924c51b3cafe2d7fcd76
Louis D'hauwe <louisdhauwe@silverfox.be>
2017-02-11 16:53:55 +0100

    Initial Commit

---
+++ PanelKit/PanelKit.h
@@ -0,0 +1,19 @@
+//
+//  PanelKit.h
+//  PanelKit
+//
+//  Created by Louis D'hauwe on 11/02/2017.
+//  Copyright © 2017 Silver Fox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PanelKit.
+FOUNDATION_EXPORT double PanelKitVersionNumber;
+
+//! Project version string for PanelKit.
+FOUNDATION_EXPORT const unsigned char PanelKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PanelKit/PublicHeader.h>
+
+
