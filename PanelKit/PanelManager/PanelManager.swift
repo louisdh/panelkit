a74c68e94920facdf76d6e8bc7b45d4357ee0ef7
Louis D'hauwe <louisdhauwe@silverfox.be>
2017-02-21 19:07:38 +0100

    Added PanelManager documentation

--- PanelKit/PanelManager/PanelManager.swift
+++ PanelKit/PanelManager/PanelManager.swift
@@ -11,21 +11,35 @@ import UIKit
 
 public protocol PanelManager: PanelViewControllerDelegate, PanelsFullscreenTransitionDelegate, PanelContentViewControllerDelegate {
 	
+	/// The panels to be managed.
 	var panels: [PanelViewController] { get }
 	
+	/// Allow floating panels
 	var allowFloatingPanels: Bool { get	}
 	
+	/// Allow panels to pin to either the left or right side, 
+	/// resizing ```panelContentView``` when a panel is pinned.
 	var allowPanelPinning: Bool { get }
-		
+	
+	/// The view in which the panels may be dragged around.
 	var panelContentWrapperView: UIView { get }
 	
+	/// The content view, which will be moved/resized when panels pin.
 	var panelContentView: UIView { get }
 	
-	/// Default implementation is ```LogLevel.none```
+	/// Default implementation is ```LogLevel.none```.
 	var panelManagerLogLevel: LogLevel { get }
 	
+	/// This will be called when a panel is pinned or unpinned.
 	func didUpdatePinnedPanels()
 	
+	/// Drag insets for panel.
+	///
+	/// E.g. a positive top inset will change the minimum y value
+	/// a panel can be dragged to inside ```panelContentWrapperView```.
+	///
+	/// - Parameter panel: The panel for which to provide insets.
+	/// - Returns: Edge insets.
 	func dragInsets(for panel: PanelViewController) -> UIEdgeInsets
 	
 }
