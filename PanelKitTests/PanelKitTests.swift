3454d6f620a61bcb788a267169dac7085ec56088
Louis D'hauwe <louisdhauwe@silverfox.be>
2017-03-11 00:23:24 +0100

    Swiftlint autocorrect

--- PanelKitTests/PanelKitTests.swift
+++ PanelKitTests/PanelKitTests.swift
@@ -16,15 +16,15 @@ class PanelKitTests: XCTestCase {
 
     override func setUp() {
         super.setUp()
-		
+
 		viewController = ViewController()
 		navigationController = UINavigationController(rootViewController: viewController)
 		navigationController.view.frame = CGRect(origin: .zero, size: CGSize(width: 1024, height: 768))
-	
+
 		let window = UIWindow(frame: UIScreen.main.bounds)
 		window.rootViewController = navigationController
 		window.makeKeyAndVisible()
-	
+
 		XCTAssertNotNil(navigationController.view)
 		XCTAssertNotNil(viewController.view)
 
@@ -66,119 +66,118 @@ class PanelKitTests: XCTestCase {
 		}
 
     }
-	
+
 	func testExpose() {
-		
+
 		let mapPanel = viewController.mapPanelVC!
 		let textPanel = viewController.textPanelVC!
-		
+
 		let exp = self.expectation(description: "expose")
-		
+
 		viewController.showMapPanelFromBarButton {
-			
+
 			assert(mapPanel.isPresentedAsPopover)
-			
+
 			self.viewController.toggleFloatStatus(for: mapPanel, completion: {
-				
+
 				self.viewController.showTextPanelFromBarButton {
 
 					self.viewController.toggleFloatStatus(for: textPanel, completion: {
 
 						assert(mapPanel.isFloating)
 						assert(textPanel.isFloating)
-						
+
 						self.viewController.enterExpose()
-						
+
 						assert(mapPanel.isInExpose)
 						assert(textPanel.isInExpose)
-						
+
 						self.viewController.exitExpose()
-						
+
 						assert(!mapPanel.isInExpose)
 						assert(!textPanel.isInExpose)
-						
+
 						exp.fulfill()
-						
+
 					})
-					
+
 				}
-				
+
 			})
-			
+
 		}
-		
+
 		waitForExpectations(timeout: 10.0) { (error) in
 			if let error = error {
 				XCTFail(error.localizedDescription)
 			}
 		}
-		
+
 	}
-	
+
 	func testPinned() {
 
 		let mapPanel = viewController.mapPanelVC!
-		
+
 		let exp = self.expectation(description: "pinned")
-		
+
 		viewController.showMapPanelFromBarButton {
-			
+
 			assert(mapPanel.isPresentedAsPopover)
-			
+
 			self.viewController.toggleFloatStatus(for: mapPanel, completion: {
-				
+
 				self.viewController.didEndDrag(mapPanel, toEdgeOf: .right)
 
 				assert(mapPanel.isPinned)
 				assert(self.viewController.panelPinnedRight == mapPanel)
-				
-				
+
 				self.viewController.didDragFree(mapPanel)
 				assert(!mapPanel.isPinned)
 				assert(self.viewController.panelPinnedRight == nil)
 
 				exp.fulfill()
-				
+
 			})
-			
+
 		}
-		
+
 		waitForExpectations(timeout: 10.0) { (error) in
 			if let error = error {
 				XCTFail(error.localizedDescription)
 			}
 		}
-		
+
 	}
-	
+
 	func testKeyboard() {
 
 		let textPanel = viewController.textPanelVC!
-		
+
 		let exp = self.expectation(description: "keyboard")
-		
+
 		viewController.showTextPanelFromBarButton {
-			
+
 			assert(textPanel.isPresentedAsPopover)
-			
+
 			self.viewController.toggleFloatStatus(for: textPanel, completion: {
-				
+
 				let textView = self.viewController.textPanelContentVC.textView
-				
+
 				textView!.becomeFirstResponder()
-				
+
 				assert(textView!.isFirstResponder)
-				
+
 				textView!.resignFirstResponder()
 
 				assert(!textView!.isFirstResponder)
 
 				exp.fulfill()
-				
+
 			})
-			
+
 		}
-		
+
 		waitForExpectations(timeout: 10.0) { (error) in
 			if let error = error {
 				XCTFail(error.localizedDescription)
@@ -187,199 +186,195 @@ class PanelKitTests: XCTestCase {
 	}
 
 	func testOffOnScreen() {
-		
+
 		let mapPanel = viewController.mapPanelVC!
-		
+
 		let exp = self.expectation(description: "offOnScreen")
-		
+
 		viewController.showMapPanelFromBarButton {
-			
+
 			assert(mapPanel.isPresentedAsPopover)
-			
+
 			self.viewController.toggleFloatStatus(for: mapPanel, completion: {
-				
+
 				// Move off screen
-				
+
 				self.viewController.panelsPrepareMoveOffScreen()
 				self.viewController.panelsMovePanelOffScreen()
-				
+
 				self.viewController.view.layoutIfNeeded()
 				self.viewController.panelsCompleteMoveOffScreen()
-				
+
 				let vcFrame = self.viewController.view.bounds
 				let mapPanelFrame = mapPanel.view.frame
-				
+
 				assert(!vcFrame.intersects(mapPanelFrame))
-				
-				
+
 				// Move on screen
-				
+
 				self.viewController.panelsPrepareMoveOnScreen()
 				self.viewController.panelsMovePanelOnScreen()
-				
+
 				self.viewController.view.layoutIfNeeded()
 				self.viewController.panelsCompleteMoveOnScreen()
 
 				let mapPanelFrameOn = mapPanel.view.frame
 				assert(vcFrame.intersects(mapPanelFrameOn))
 
-				
 				exp.fulfill()
-				
+
 			})
-			
+
 		}
-		
+
 		waitForExpectations(timeout: 10.0) { (error) in
 			if let error = error {
 				XCTFail(error.localizedDescription)
 			}
 		}
-		
+
 	}
-	
+
 	func testClosing() {
-		
+
 		let mapPanel = viewController.mapPanelVC!
-		
+
 		let exp = self.expectation(description: "closing")
-		
+
 		viewController.showMapPanelFromBarButton {
-			
+
 			assert(mapPanel.isPresentedAsPopover)
-			
+
 			self.viewController.toggleFloatStatus(for: mapPanel, completion: {
-				
+
 				assert(mapPanel.isFloating)
 
 				self.viewController.close(mapPanel)
-				
+
 				assert(!mapPanel.isFloating)
-				
+
 				exp.fulfill()
-				
+
 			})
-						
+
 		}
-		
+
 		waitForExpectations(timeout: 10.0) { (error) in
 			if let error = error {
 				XCTFail(error.localizedDescription)
 			}
 		}
-		
+
 	}
-	
+
 	func testClosingAllFloating() {
-		
+
 		let mapPanel = viewController.mapPanelVC!
-		
+
 		let exp = self.expectation(description: "closing")
-		
+
 		viewController.showMapPanelFromBarButton {
-			
+
 			self.viewController.toggleFloatStatus(for: mapPanel, completion: {
-				
+
 				assert(mapPanel.isFloating)
-				
+
 				self.viewController.closeAllFloatingPanels()
-				
+
 				assert(!mapPanel.isFloating)
-				
+
 				exp.fulfill()
-				
+
 			})
-			
+
 		}
-		
+
 		waitForExpectations(timeout: 10.0) { (error) in
 			if let error = error {
 				XCTFail(error.localizedDescription)
 			}
 		}
-		
+
 	}
-	
+
 	func testClosingAllPinned() {
-		
+
 		let mapPanel = viewController.mapPanelVC!
-		
+
 		let exp = self.expectation(description: "closing")
-		
+
 		viewController.showMapPanelFromBarButton {
-			
+
 			self.viewController.toggleFloatStatus(for: mapPanel, completion: {
-				
+
 				self.viewController.didEndDrag(mapPanel, toEdgeOf: .right)
-				
+
 				assert(mapPanel.isPinned)
 				assert(self.viewController.panelPinnedRight == mapPanel)
-				
+
 				self.viewController.closeAllPinnedPanels()
-				
+
 				assert(!mapPanel.isPinned)
 				assert(self.viewController.panelPinnedRight == nil)
-				
+
 				exp.fulfill()
-				
+
 			})
-			
+
 		}
-		
+
 		waitForExpectations(timeout: 10.0) { (error) in
 			if let error = error {
 				XCTFail(error.localizedDescription)
 			}
 		}
-		
+
 	}
-	
+
 	func testDragToPin() {
-		
+
 		let mapPanel = viewController.mapPanelVC!
-		
+
 		let exp = self.expectation(description: "dragToPin")
-		
+
 		viewController.showMapPanelFromBarButton {
-			
+
 			assert(mapPanel.isPresentedAsPopover)
-			
+
 			self.viewController.toggleFloatStatus(for: mapPanel, completion: {
-				
+
 				let from = mapPanel.view.center
 				let toX = self.viewController.view.bounds.width - mapPanel.contentViewController!.view.bounds.width/2
 				let to = CGPoint(x: toX, y: mapPanel.view.center.y)
 				mapPanel.panelNavigationController.moveWithTouch(from: from, to: to)
 				self.viewController.view.layoutIfNeeded()
-				
+
 				mapPanel.panelNavigationController.moveWithTouch(from: to, to: to)
 
 				mapPanel.didEndDrag()
-				
+
 				assert(mapPanel.isPinned)
 				assert(self.viewController.panelPinnedRight == mapPanel)
-				
-				
+
 				mapPanel.panelNavigationController.moveWithTouch(from: to, to: from)
 				self.viewController.view.layoutIfNeeded()
 				mapPanel.didEndDrag()
 
 				assert(!mapPanel.isPinned)
 				assert(self.viewController.panelPinnedRight == nil)
-				
-				
+
 				exp.fulfill()
 
 			})
-			
+
 		}
-		
+
 		waitForExpectations(timeout: 10.0) { (error) in
 			if let error = error {
 				XCTFail(error.localizedDescription)
 			}
 		}
-		
+
 	}
-	
+
 }
