3b93bbed54bc3616d6a6c206c72d82e9fcf0ee03
Louis D'hauwe <louisdhauwe@silverfox.be>
2017-11-15 21:26:02 +0100

    Add documentation

--- PanelKit/PanelManager/PanelManager.swift
+++ PanelKit/PanelManager/PanelManager.swift
@@ -8,22 +8,41 @@
 
 import UIKit
 
+/// The PanelManager protocol contains the necessary settings for letting
+/// panels float and pin. It also contains callbacks for certain actions triggered by panels.
+///
+/// Typically the `PanelManager` protocol is implemented on a `UIViewController` subclass.
+/// If not, you should specify the `managerViewController` property.
 public protocol PanelManager: class {
 
 	/// The ```UIViewController``` that manages the panels and contains
 	/// ```panelContentWrapperView``` and ```panelContentView```.
+	///
+	/// When the PanelManager protocol is implemented on a `UIViewController` subclass
+	/// this property returns "self" by default.
 	var managerViewController: UIViewController { get }
 
 	/// The panels to be managed.
 	var panels: [PanelViewController] { get }
 
-	/// Allow floating panels
+	/// Controls wether panels are allowed to float (be dragged around).
+	/// If this property returns true: the panel will automatically provide a UIBarButtonItem
+	/// to make itself float when shown in a popover, as well as a close button (to close itself) while it's floating.
+	/// 
+	/// The default implementation returns true if `panelContentWrapperView.bounds.width > 800`.
 	var allowFloatingPanels: Bool { get	}
 
-	/// Allow panels to pin to either the left or right side,
-	/// resizing ```panelContentView``` when a panel is pinned.
+	/// Controls wether panels are allowed to be pinned to either the left or right side.
+	/// The ```panelContentView``` is resized when a panel is pinned.
+	///
+	/// The default implementation returns true if `panelContentWrapperView.bounds.width > 800`.
 	var allowPanelPinning: Bool { get }
 
+	/// Controls the number of panels that may be pinned to a side.
+	///
+	/// The default implementation returns 1.
+	/// - Parameter side: A side where panels can be pinned to.
+	/// - Returns: Maximum number of panels that may be pinned to `side`.
 	func maximumNumberOfPanelsPinned(at side: PanelPinSide) -> Int
 	
 	/// The view in which the panels may be dragged around.
@@ -36,6 +55,7 @@ public protocol PanelManager: class {
 	var panelManagerLogLevel: LogLevel { get }
 
 	/// This will be called when a panel is pinned or unpinned.
+	/// The default implementation is an empty function.
 	func didUpdatePinnedPanels()
 
 	/// Drag insets for panel.
@@ -51,9 +71,11 @@ public protocol PanelManager: class {
 	var exposeOverlayBlurEffect: UIBlurEffect { get }
 
 	/// Called when exposé is about to be entered.
+	/// The default implementation is an empty function.
 	func willEnterExpose()
 
 	/// Called when exposé is about to be exited.
+	/// The default implementation is an empty function.
 	func willExitExpose()
 
 }
